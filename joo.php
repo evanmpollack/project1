3d4f6127d482f257ce81f806c626b2fb7a27394a
Joo Eon Park <jpark174@u.rochester.edu>
2020-07-03 07:36:15 +0900

    Added my lab01 and HTML edits
    - added lab01-joo to folder
    - added content to joo.php
    - added images to images folder
    - consistent semantics used

--- joo.php
+++ joo.php
@@ -22,6 +22,57 @@
 
 		<main>
 			<h2> A Little About Joo Eon </h2>
+
+			<section>
+				<h3> Summary </h3>
+
+				<figure>
+					<img src="images/me.jpg" alt="me">
+					<figcaption>Summer 2019.</figcaption>
+				</figure>
+
+				<p>This is a page about me. I am from South Korea, and was born and raised there my entire life. I'm fluent in both English and Korean (and speak a bit of French)! I'm going talk about some of the things I like.</p>
+
+			</section>
+
+			<article> 
+				<section>
+
+					<h3> Art </h3>
+					<p>I've always had great interest in art and design. One of my favorite things to do is go to museums and galleries. To keep my interest in art alive, I took a course on printmaking last semester. This was the first time I did printmaking, and it was a lot of fun. This piece was inspired by M.C. Escher's work. This type of pattern is called a tessellation. Linoleum blocks were carved, inked, and pressed onto the paper. This technique is called relief printing.</p>
+
+					<figure>
+						<img src="images/print.jpg" alt="my_art">
+						<figcaption><em>Symbiosis</em>, relief print.</figcaption>
+					</figure>
+				
+				</section>
+
+				<section>
+
+					<h3> Music </h3>
+					<p>I've always been in close proximity to music, mainly due to my dad's career in the musical world. I most closely relate to rock music and the electric guitar, having listened to various rock genres and bands since I was young. I most fondly recall listening to Jimi Hendrix in my dad's car. At the time, I had no regard for what I was listening to, but as I grew up I developed a great appreciation for Hendrix and music in general.</p>
+
+					<figure>
+						<img src="images/guitar.jpg" alt="playing_guitar">
+						<figcaption>Playing guitar in studio.</figcaption>
+					</figure>
+
+				</section>
+
+				<section>
+
+					<h3> My Cat </h3>
+					<p>My cat's name is Haku. She is named after the character from the film, <em>Spirited Away</em>. Story: We named our cat after Haku from the film because we thought he had blue eyes, just like our cat. Turns out he doesn't... She is a 9 year old Siamese cat, though she will be turning 10 on July 15. She's needy and is sometimes an ass (as cats typically are), but she is quite cute nonetheless.</p>
+
+					<figure>
+						<img src="images/cat.png" alt="cat_in_hole">
+						<figcaption>Haku in a hole.</figcaption>
+					</figure>
+
+				</section>
+			</article>
+
 		</main>
 
 		<footer>
