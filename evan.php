dfe7270b91c5e27ed3b2be6c274ec2fe422c8293
jmandel5 <67707013+jmandel5@users.noreply.github.com>
2020-07-02 16:42:13 -0400

    working on jack.php

---
+++ evan.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> project1 - Evan's Bio</title>
+</head>
+<body>
+	<div class="container">
+		<h1> A little about Jack</h1>
+
+
+
+
+
+
+
+	</div> <!-- container -->
+</body>
+</html>
+
---
+++ jack.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> project1 - Jack's Bio</title>
+</head>
+<body>
+	<div class="container">
+		<h1> A little about Jack </h1>
+		<div> Artist - Cellist - Fisherman </div>
+
+		<h2> Summary </h2>
+
+		<p> Welcome to small personalized website about Jack M. and my various things I like to do. Outside of my majors (Math and Computer Science), I enjoy passing the time with some of my favorite hobbies. These hobbies include art, cello, and fishing. Click on each of the links on the left to learn more about some of my hobbies. </p>
+
+		
+
+
+
+
+
+
+
+	</div> <!-- container -->
+</body>
+</html>
