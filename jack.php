dddf6c46d38b7398e54c6f2c6a3fbe388bd32e79
jmandel5 <67707013+jmandel5@users.noreply.github.com>
2020-07-02 17:04:35 -0400

    working on jack.php

--- jack.php
+++ jack.php
@@ -6,14 +6,60 @@
 </head>
 <body>
 	<div class="container">
-		<h1> A little about Jack </h1>
-		<div> Artist - Cellist - Fisherman </div>
+		<header>
+			<h1> A little about Jack </h1>
+			<div> Artist - Cellist - Fisherman </div>
+		</header>
 
-		<h2> Summary </h2>
+		<section>
+			<h2> Summary </h2>
 
-		<p> Welcome to small personalized website about Jack M. and my various things I like to do. Outside of my majors (Math and Computer Science), I enjoy passing the time with some of my favorite hobbies. These hobbies include art, cello, and fishing. Click on each of the links on the left to learn more about some of my hobbies. </p>
+			<figure>
+				<img src="images/face.png" alt="me">
+				<figcaption> moi </figcaption>
+			</figure>
 
-		
+			<p> Welcome to a small bio page about me (Jack) and my various things I like to do. Outside of my majors (Math and Computer Science), I enjoy passing the time with some of my favorite hobbies. These hobbies include art, cello, and fishing. Scroll down to learn more about me.  </p>
+		</section>
+
+		<main> 
+			<section>
+				<h2> Art </h2>
+				<p> For many years as a child, I took art lessons, and recently for the past two years over the summer, I have taught art lessons at studios near me. I enjoy painting the most and more specifically I like watercolor and acrylic. Many of my favorite things to paint include landscapes and still life.   </p>
+
+				<figure>
+					<img src="images/art.jpg" alt="art">
+					<figcaption> A painting I did of me fishing </figcaption>
+				</figure>
+
+			</section>
+
+			<section>
+
+				<figure>
+					<img src="images/cello.png" alt="cello">
+					<figcaption> A picture of a cello </figcaption>
+				</figure>
+
+				<h2> Cello </h2>
+				<p> I have played cello for many years and enjoy listening to classical music. Wanting to continue playing cello on a consistent basis during college, I joined the University's Chamber Orchestra and have met people who appreciate classical as much as I do. I have even joined a small string ensemble with a few friends for the upcoming semester. </p>
+
+			</section>
+
+			<section>
+				<h2> Fishing </h2>
+				<p> I love to go fishing in the ponds around my home, as well as exploring new waters when I am traveling. I am drawn to the unpredicatability of fishing and the adventure of exploring new areas I have never fished before. Fishing has taught me a lot about patience and persistence. The feeling of fighting a large fish never gets old.    </p>
+
+				<figure>
+					<img src="images/fishing.png" alt="fishing">
+					<figcaption> fishing rods </figcaption>
+				</figure>
+
+			</section>
+
+		</main>
+
+
 
 
 
