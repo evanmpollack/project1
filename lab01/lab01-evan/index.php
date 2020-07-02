331f0b3f3c08f5b5ebeed173b3194bb669fad96a
evanmpollack <50115376+evanmpollack@users.noreply.github.com>
2020-07-02 14:38:23 -0400

    Folder for Original Lab01's
    Put your lab01's in this folder. 
    NAMING CONVENTION: lab01-firstname

---
+++ lab01/lab01-evan/index.php
@@ -0,0 +1,10 @@
+<?php include "inc/above-main.php"?>
+
+    <main>
+        <p id="border">I am a student at the University of Rochester, which is located in Rochester, NY. My major
+            is undecided, but I plan on minoring in computer science. I grew
+            up in the Washington D.C. area. I have been involved in activities such as
+            running competitively and snowboarding. </p>
+    </main>
+
+<?php include "inc/below-main.php"?>
