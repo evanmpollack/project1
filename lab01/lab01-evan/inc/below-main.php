4fbb6e84fd7ba2b4960ebd1e4f8510f6f507f65c
Evan Pollack <evanmpollack@gmail.com>
2020-07-05 17:42:26 -0400

    Optimized v1

--- lab01/lab01-evan/inc/below-main.php
+++
@@ -1,6 +0,0 @@
-    <footer>CSC 174: Advanced Front-end Web Design and Development</footer>
-
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-    <script src="js/menu-highlighter.js"></script>
-</body>
-</html>
--- lab01/lab01-joo/inc/nav.php
+++
@@ -1,8 +0,0 @@
-<nav class="menu">
-	<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="art.php">Art</a></li>
-			<li><a href="music.php">Music</a></li>
-			<li><a href="cat.php">My Cat</a></li>
-	</ul>
-</nav>
